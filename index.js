f493bbad1573d5bd511036867cf46896b18d7fcc
youssef Tamer <yousftamer11.com>
2024-12-24 16:35:21 +0200

    add CORS

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-//import ngrok from '@ngrok/ngrok'
 import dotenv from 'dotenv'
 import path from 'path'
 import express from 'express'
+import cors from 'cors'
 import { connectDB } from './db/connection.js'
 import { initApp } from './src/initApp.js'
 dotenv.config({ path: path.resolve('./config/.env') })
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Allow requests from this origin
+      methods: "GET,POST,PUT,DELETE,OPTIONS", // Allowed HTTP methods
+      allowedHeaders: "Content-Type,Authorization", // Allowed headers in requests
+    })
+  );
+
 connectDB()
 
 initApp(app, express)
@@ -14,5 +23,3 @@ app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`))
 
 
-//ngrok.connect({addr:3000,authtoken_from_env:true})
-//.then(listener => console.log(`Ingress established at: ${listener.url()}`)); 
